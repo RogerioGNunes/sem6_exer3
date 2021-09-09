0ac499c18668fd36a1277fd5c1a4c6d609c14af9
RogerioGNunes <rg.nunes2013@gmail.com>
2021-09-09 18:27:12 +0000

    treino exercício 3

---
+++ exer3.java
@@ -0,0 +1,12 @@
+public class exer3{
+
+    public static void main (String args[]){
+
+        for(int n=1; n<=4;n++){
+            for(int i=1;i<=n;i++){
+                System.out.print(n*i +"  ");
+            }
+            System.out.println();
+        }
+    }
+}
